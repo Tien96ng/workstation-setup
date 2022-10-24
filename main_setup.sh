ad28c38a7814ba219508aa689d6226de5cdceee6
tien96ng <tien96ng@gmail.com>
2022-10-24 05:49:22 -0700

    Add OS checking to main_setup

--- main_setup.sh
+++ main_setup.sh
@@ -1,4 +1,40 @@
  #!/bin/bash
-cat Linux/apps.json
+
+### CHECKING OS
+ OS="Unknown"
+ LINUX=false
+ echo "Checking device OS...."
+
+case "$OSTYPE" in
+  solaris*) OS="SOLARIS" ;;
+  darwin*)  OS="OSX" ;; 
+  linux*)   OS="LINUX" ;;
+  bsd*)     OS="BSD" ;;
+  msys*)    OS="WINDOWS" ;;
+  cygwin*)  OS="WINDOWS" ;;
+  *)        OS="Unknown" ;;
+esac
+
+checkLinuxType() {
+    tmp=$(grep "^ID_LIKE" /etc/os-release)
+    echo "${tmp}"
+
+}
+
+if [ "$OS" = "LINUX" ]; then
+    echo "OS is Linux based. Checking Linux version/type..."
+    OS=$(checkLinuxType)
+    LINUX=true
+fi
+
+echo ""
+echo "Current OS: "${OS:8}
+
+if [ LINUX ]; then
+    echo "Is Linux"
+else
+    echo "Not Linux"
+fi
+
 
 $SHELL
